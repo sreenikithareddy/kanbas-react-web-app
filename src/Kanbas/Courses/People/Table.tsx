786f317f1d1761faf34d8d6c4aa1d770b3019d2f
sreenikithareddy <sreenikithareddy@gmail.com>
2024-07-26 17:34:57 +0530

    A6

--- src/Kanbas/Courses/People/Table.tsx
+++ src/Kanbas/Courses/People/Table.tsx
@@ -1,17 +1,64 @@
 import React, { useState, useEffect } from "react";
 import * as client from "./client";
+import { Link, useParams } from "react-router-dom";
+import PeopleDetails from "./Details";
+import { FaPlus } from "react-icons/fa";
 export default function PeopleTable() {
+  const {cid} = useParams();
   const [users, setUsers] = useState<any[]>([]);
   const fetchUsers = async () => {
     const users = await client.findAllUsers();
-    console.log(users);
     setUsers(users);
   };
   useEffect(() => {
     fetchUsers();
   }, []);
+  const [role, setRole] = useState("");
+  const filterUsersByRole = async (role: string) => {
+    setRole(role);
+    if (role) {
+      const users = await client.findUsersByRole(role);
+      setUsers(users);
+    } else {
+      fetchUsers();
+    }
+  };
+  const [name, setName] = useState("");
+  const filterUsersByName = async (name: string) => {
+    setName(name);
+    if (name) {
+      const users = await client.findUsersByPartialName(name);
+      setUsers(users);
+    } else {
+      fetchUsers();
+    }
+  };
+  const createUser = async () => {
+    const user = await client.createUser({
+      firstName: "New",
+      lastName: `User${users.length + 1}`,
+      username: `newuser${Date.now()}`,
+      password: "password123",
+      section: "S101",
+      role: "STUDENT",
+    });
+    setUsers([...users, user]);
+  };
+
   return (
     <div id="wd-people-table">
+       <button onClick={createUser} className="float-end btn btn-danger wd-add-people">
+        <FaPlus className="me-2" />
+        People
+      </button>
+      <PeopleDetails fetchUsers={fetchUsers}/>
+      <input onChange={(e) => filterUsersByName(e.target.value)} placeholder="Search people"
+             className="form-control float-start w-25 me-2 wd-filter-by-name" />
+       <select value={role} onChange={(e) =>filterUsersByRole(e.target.value)}
+              className="form-select float-start w-25 wd-select-role" >
+        <option value="">All Roles</option>        <option value="STUDENT">Students</option>
+        <option value="TA">Assistants</option>     <option value="FACULTY">Faculty</option>
+      </select>
       <table className="table table-striped">
         <thead>
           <tr>
@@ -19,11 +66,13 @@ export default function PeopleTable() {
           </tr>
         </thead>
         <tbody>
-          {users.map((user: any) => (  
+          {users.map((user: any) => (
             <tr key={user._id}>
               <td className="wd-full-name text-nowrap">
+                <Link to = {`/Kanbas/Courses/${cid}/People/${user._id}`}>
                 <span className="wd-first-name">{user.firstName}</span>
                 <span className="wd-last-name">{user.lastName}</span>
+                </Link>
               </td>
               <td className="wd-login-id">{user.loginId}</td>
               <td className="wd-section">{user.section}</td>
@@ -37,4 +86,3 @@ export default function PeopleTable() {
     </div>
   );
 }
-
